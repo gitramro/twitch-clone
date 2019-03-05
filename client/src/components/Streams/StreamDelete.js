cb824cd400df7e8e58aaa396fa62d1354ce6b76a
Ramon Romero <ramon.osc93@gmail.com>
2019-03-05 15:53:06 -0700

    add modal with react portal

--- client/src/components/Streams/StreamDelete.js
+++ client/src/components/Streams/StreamDelete.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import Modal from '../Modal';
+import history from '../../history';
+import {fetchStream} from '../../actions'
 
-const   StreamDelete = () => {
-  return <div>  StreamDelete</div>
-};
+class StreamDelete extends React.Component{
 
-export default  StreamDelete;
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id)
+  }
+
+  renderActions(){
+    return(
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
+
+  renderContent(){
+    if(!this.props.stream){
+      return 'Are you sure you want to return this stream?'
+    }
+
+    return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`;
+  }
+
+
+render(){
+  return(
+    <Modal
+      title="Delete Stream"
+      content={this.renderContent()}
+      actions={this.renderActions()}
+      onDismiss={()=>history.push('/')}
+    />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps)=>{
+  return{stream:state.streams[ownProps.match.params.id]}
+}
+
+export default  connect(mapStateToProps,{fetchStream})(StreamDelete);
